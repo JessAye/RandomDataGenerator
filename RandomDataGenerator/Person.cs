a382f793c8b372eefb8c794858c7096717a690d7
MaltyGreen <60976774+MaltyGreen@users.noreply.github.com>
2022-09-11 09:52:54 -0400

    Added Variables

--- RandomDataGenerator/Person.cs
+++ RandomDataGenerator/Person.cs
@@ -1,6 +1,12 @@
-﻿namespace RandomDataGenerator;
+namespace RandomDataGenerator;
 
 public class Person
 {
-    
+    private string firstName;
+    private string lastName;
+    private DateTime birthDate;
+    private double Age;
+    private int SSN;
+    private int PhoneNumber;
+    private Person[] Dependents;
 }
