5a0d558a125a4dba2100cafbd4a264f84e10f47e
MaltyGreen <60976774+MaltyGreen@users.noreply.github.com>
2022-09-12 16:35:35 -0400

    Update Person.cs
    added a lot of things to the person class including gets and inits
    its something all right

--- RandomDataGenerator/Person.cs
+++ RandomDataGenerator/Person.cs
@@ -1,12 +1,77 @@
+using System.Security.Cryptography;
+using System.Threading.Channels;
+
 namespace RandomDataGenerator;
 
 public class Person
 {
-    private string firstName;
-    private string lastName;
-    private DateTime birthDate;
-    private double Age;
-    private int SSN;
-    private int PhoneNumber;
-    private Person[] Dependents;
+    Random rand = new Random();
+
+    private string[] _arrayOfFirstNames = new String[10]
+        { "Roy", "Craig", "Rose", "Milton", "Peter", "Brian", "Hugh", "Greg", "Sam", "Fred" };
+
+    public string firstName;
+    public Enum lastName;
+    public DateTime birthDate;
+    public double age;
+    public SSN ssNum;
+    public Phone phoneNumber;
+    //public Person[] Dependents;
+
+
+    public override string ToString()
+    {
+        return string.Format("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}",
+            firstName,
+            lastName,
+            age,
+            birthDate.ToString("MM/dd/yyyy"),
+            ssNum,
+            phoneNumber,
+            "Dependants");
+        //Dependents.Length);
+    }
+
+ 
+    public string FirstName
+    {
+        get { return firstName; }
+        init { firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return firstName; }
+        init { firstName = value; }
+    }
+
+    public DateTime BirthDate
+    {
+        get { return birthDate; }
+        init { birthDate = value; }
+    }
+
+    public double Age
+    {
+        get { return age; }
+        init { age = value; }
+    }
+
+    public Phone PhoneNumber
+    {
+        get { return phoneNumber; }
+        init { phoneNumber = value; }
+    }
+
+    public SSN SSNUM
+    {
+        get { return ssNum; }
+        init { ssNum = value; }
+    }
+
+    public string[] ArrayOfFirstNames
+    {
+        get { return _arrayOfFirstNames; }
+        init { _arrayOfFirstNames = value; }
+    }
 }
